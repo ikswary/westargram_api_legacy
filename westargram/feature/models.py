80ed9ff1dd841f2ad54632beed99872e40945692
ikswary <ikswary@naver.com>
2020-04-04 19:18:09 +0900

    correct : corrected UserData's comment field's name to password

--- westargram/feature/models.py
+++ westargram/feature/models.py
@@ -10,4 +10,4 @@ class Comments(models.Model):
 
 class UserData(models.Model):
     userid = models.CharField(max_length=20)
-    comment = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
