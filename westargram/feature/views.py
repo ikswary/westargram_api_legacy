11779636b62d3f3c53405961a58c31707ace0893
ikswary <ikswary@naver.com>
2020-04-04 16:34:51 +0900

    add : added CommentView

--- westargram/feature/views.py
+++ westargram/feature/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from .models import Comments
+
+
+class CommentView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        Comments(
+            username=data['username'],
+            comment=data['comment']
+        ).save()
+
+        return JsonResponse({'message': 'Comment Upload Success'}, status=200)
